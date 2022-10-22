1567cf16d67f89e70007505fb703f4d735153f58
lazysomedayz <lazysomedayzz@hotmail.com>
2022-10-22 23:59:32 +0700

    Add project files.

---
+++ Exercise4/Box.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    internal class Box
+    {
+        public double width { get; private set; }
+        public double height { get; private set; }
+        public double length { get; private set; }
+
+        public Box(double width, double height, double length)
+        {
+            this.width = width;
+            this.height = height;
+            this.length = length;
+        }
+
+        public Box(double size)
+        {
+            width = size;
+            height = size;
+            length = size;
+        }
+
+        public Box(Box oldBox)
+        {
+            width = oldBox.width;
+            height = oldBox.height;
+            length = oldBox.length;
+        }
+
+
+        public double faceArea()
+        {
+            return width * height;
+        }
+
+        public double topArea()
+        {
+            return width * length;
+        }
+
+        public double sideArea()
+        {
+            return height * length;
+        }
+
+        public double area()
+        {
+            return 2 * faceArea() + 2 * topArea() + 2 * sideArea();
+        }
+    }
+}
---
+++ Exercise4/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box first_box = new Box(width: 5.12, height: 5.74, length: 5.2);
+            Box second_box = new Box(size: 10);
+            Box third_box = new Box(oldBox: first_box);
+
+            Box[] boxes = { first_box, second_box, third_box };
+
+            calculateArea(boxes);
+        }
+        static void calculateArea(Box[] boxes)
+        {
+            foreach (Box box in boxes)
+            {
+                int index = Array.IndexOf(boxes, box) + 1;
+                Console.WriteLine("Box {0}'s face area is: {1}", index, box.faceArea());
+                Console.WriteLine("Box {0}'s side area is: {1}", index, box.sideArea());
+                Console.WriteLine("Box {0}'s top area is: {1}", index, box.topArea());
+                Console.WriteLine("Box {0}'s area is: {1}", index, box.area());
+                Console.WriteLine("---------------------------------------------------");
+            }
+        }
+    }
+}
